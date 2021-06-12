64ba79bef73b9eb34e3511afd81291315fe3f6c8
Malek-Haddar <73005283+Malek-Haddar@users.noreply.github.com>
2021-06-12 02:37:39 +0100

    hero & projects section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        The purpose of this portfolio is to present my self to the world and
+        take my development skills to the next level.
+      </SectionText>
+      <Button onClick={() => (window.location = 'https://google.com')}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
